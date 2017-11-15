1dc8701d5b9c55a085c3dbe0b486d21a3b952a5e
Praveen <praveenh@syncfusion.com>
2017-11-15 15:44:30 +0530

    Added the Sample for id card reader

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace IDCardReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(748, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DoOCR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(657, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(443, 390);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(32, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(579, 390);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "BrowseImage";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(32, 44);
+            this.trackBar1.Maximum = 225;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(485, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Threshold";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1139, 494);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.OCR;
+using Emgu.Util;
+
+using System.IO;
+
+namespace IDCardReader
+{
+    public partial class Form1 : Form
+    {
+
+        string fileName = Path.GetFullPath("../../Data/idcard.jpg");
+        Image<Gray, Byte> sourceImage;
+        Image<Gray, Byte> imgResize;
+        Image<Gray, Byte> img_gray;
+        private Tesseract _ocr;
+        public Form1()
+        {
+            InitializeComponent();
+            _ocr = new Tesseract(Path.GetFullPath("../../Data/"), "eng", OcrEngineMode.TesseractLstmCombined);
+            sourceImage = new Image<Gray, Byte>(fileName);
+            Bitmap bit = sourceImage.Resize(pictureBox1.Width, pictureBox1.Height, Inter.Linear).ToBitmap();
+            pictureBox1.Image = bit;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            //Resize the image to higherquality
+          
+            imgResize = sourceImage.Resize(2, Inter.Linear);
+          
+
+            if (trackBar1.Value > 0)
+                //applying smooth and converting to binary
+                img_gray = imgResize.Convert<Gray, Byte>().SmoothGaussian(5).ThresholdBinary(new Gray(trackBar1.Value), new Gray(255));
+            else
+                img_gray = imgResize.Convert<Gray, Byte>().SmoothGaussian(5);
+
+            _ocr.SetImage(img_gray);
+            _ocr.Recognize();
+
+            richTextBox1.Text = _ocr.GetUTF8Text();
+            img_gray.Dispose();
+            imgResize.Dispose();
+          
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image!=null)
+            pictureBox1.Image.Dispose();
+            if (sourceImage != null)
+            {
+                img_gray = sourceImage.Convert<Gray, Byte>().SmoothGaussian(5).ThresholdBinary(new Gray((sender as TrackBar).Value), new Gray(255));
+                Bitmap image = img_gray.Resize(pictureBox1.Width, pictureBox1.Height, Inter.Linear).ToBitmap();
+                pictureBox1.Image = image;
+                img_gray.Dispose();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Images(*.jpg,*.png,*.jpeg)|*.jpg;*.png;*.jpeg";
+           if(dialog.ShowDialog()==DialogResult.OK)
+            {
+                fileName = dialog.FileName;
+                sourceImage = new Image<Gray, Byte>(fileName);
+            }
+            trackBar1.Value = 0;
+           
+            Bitmap bit = sourceImage.Resize(pictureBox1.Width, pictureBox1.Height, Inter.Linear).ToBitmap();
+            pictureBox1.Image = bit;
+        }
+    }
+ }
+
+
